2adebfda0c3c120a4a4aa2e4fab7b5c966ff1550
hnmarkin <hnmarkin@crimson.ua.edu>
2025-05-07 12:35:02 -0500

    More Policy UI and scriptableObject tweaks

--- Assets/_Project/Scripts/Policy/CardScriptableObject.cs
+++ Assets/_Project/Scripts/Policy/CardScriptableObject.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
 
+public enum CardType   { General, Residential, Corporate, Political }
+
+[CreateAssetMenu(menuName = "Policy/Card")]
 public class CardData : ScriptableObject
 {
     public string cardName;
+    public CardType cardType;
     [TextArea] public string description;
     public int residentialOpinion, corporateOpinion, politicalOpinion;
     public int money;
+    public int actionPoints;
 }
