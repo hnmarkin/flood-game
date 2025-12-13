bda932c24e0c9813bc4598158471a32b57ae7f6e
hnmarkin <hnmarkin@crimson.ua.edu>
2025-12-13 15:22:27 -0600

    Correct alert pop-up animation

--- Assets/_Project/UI/Scripts/Alerts/AlertAnimation.cs
+++ Assets/_Project/UI/Scripts/Alerts/AlertAnimation.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class AlertAnimation : MonoBehaviour
 {
-    [SerializeField] private GameObject alertObject;
     [SerializeField] private float slideDuration = 0.4f;
     [SerializeField] private LeanTweenType ease = LeanTweenType.easeOutCubic;
 
-    public void PlaySlideIn(Vector2 targetAnchoredPosition, float duration = -1f)
+    public void PlaySlideIn(GameObject alertObject, Vector2 targetAnchoredPosition)
     {
         if (alertObject == null) alertObject = this.gameObject;
-        float d = duration > 0 ? duration : slideDuration;
+        float d = slideDuration;
 
         RectTransform rt = alertObject.GetComponent<RectTransform>();
         if (rt != null)
@@ -26,5 +25,31 @@ public class AlertAnimation : MonoBehaviour
         LeanTween.cancel(alertObject);
         Vector3 targetWorld = new Vector3(targetAnchoredPosition.x, targetAnchoredPosition.y, alertObject.transform.localPosition.z);
         LeanTween.moveLocal(alertObject, targetWorld, d).setEase(ease);
+    }
+
+    public float PlaySlideOut(GameObject alertObject, Vector2 targetAnchoredPosition)
+    {
+        if (alertObject == null) alertObject = this.gameObject;
+        float d = slideDuration;
+
+        RectTransform rt = alertObject.GetComponent<RectTransform>();
+        if (rt != null)
+        {
+            LeanTween.cancel(alertObject);
+            float startX = rt.anchoredPosition.x;
+            LeanTween.value(alertObject, startX, targetAnchoredPosition.x, d)
+                .setEase(ease)
+                .setOnUpdate((float x) => rt.anchoredPosition = new Vector2(x, rt.anchoredPosition.y))
+                .setOnComplete(() => Destroy(alertObject));
+            return d;
+        }
+
+        // World-space fallback
+        LeanTween.cancel(alertObject);
+        Vector3 targetWorld = new Vector3(targetAnchoredPosition.x, targetAnchoredPosition.y, alertObject.transform.localPosition.z);
+        LeanTween.moveLocal(alertObject, targetWorld, d)
+            .setEase(ease)
+            .setOnComplete(() => Destroy(alertObject));
+        return d;
     }
 }
--- Assets/_Project/UI/Scripts/Alerts/AlertLoader.cs
+++ Assets/_Project/UI/Scripts/Alerts/AlertLoader.cs
@@ -1,6 +1,8 @@
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 [System.Serializable]
 
@@ -22,8 +24,11 @@ public class AlertData
 
 public class AlertLoader : MonoBehaviour
 {
+    [SerializeField] private AlertAnimation alertAnimation;
     [SerializeField] private GameObject alertPrefab;
     [SerializeField] private Transform alertParent;
+    [SerializeField] private float offsetX = 240f;
+    [SerializeField] private float finalX = 0f;
     // [SerializeField] private TMP_Text infoText;
 
     private void OnEnable() 
@@ -38,15 +43,26 @@ public class AlertLoader : MonoBehaviour
 
     private void OnAlertRaised(AlertData alertData)
     {
-        GameObject alertObj = Instantiate(alertPrefab, alertParent);
-        AlertViewer viewer = alertObj.GetComponent<AlertViewer>();
-        if (viewer != null)
-        {
-            viewer.Setup(alertData);
-        }
-        else
-        {
-            Debug.LogError("AlertViewer component not found on alert prefab.");
-        }
+        var contentRT = (RectTransform)alertParent;
+        var row = new GameObject("Row", typeof(RectTransform));
+        var rowRT = (RectTransform)row.transform;
+        rowRT.SetParent(contentRT, false);
+        rowRT.localScale = Vector3.one;
+
+        // Instantiate alert under row
+        var go = Instantiate(alertPrefab, rowRT, false);
+        var rt = go.GetComponent<RectTransform>();
+
+        var viewer = go.GetComponent<AlertViewer>();
+        viewer.Setup(alertData);
+        // Start indented
+        rt.anchoredPosition = new Vector2(offsetX, 0f);
+
+        // (Optional but often helpful) rebuild so sizes are correct before tween
+        Canvas.ForceUpdateCanvases();
+        LayoutRebuilder.ForceRebuildLayoutImmediate(contentRT);
+
+        // Animate to zero X (LeanTween example)
+        alertAnimation.PlaySlideIn(go, new Vector2(finalX, 0f));
     }
 }
